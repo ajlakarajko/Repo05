b87702317f074099a25311cbaa4ce910719427b7
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-07-14 08:25:06 +0200

    Day05

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Ajla";
+    var myLastName = "Karajko";
+    var myHomeTown = "Travnik";
+    // only change code above this line
+    return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+    };
+    }
+    console.log(main());
+    module.exports = main;
+    
