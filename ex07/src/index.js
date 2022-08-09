081a55e63ed3cf9b56ec3ebed9fd665ab07a6a2a
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-08-09 15:17:25 +0200

    Repo05

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var myName = "Ajlica";
+    var myString = "Hello, my name is " + myName + ", how are you?"; // myString is now "Hello, my name is Ajlica, how are you?"
+    // Only change code above this line
+    return myString;
+    }
+    console.log(main());
+    module.exports = main;
+    
