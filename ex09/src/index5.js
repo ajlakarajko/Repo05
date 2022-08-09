16fa12fe510b552551f7a08f63c1314c21d5bc41
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-08-09 15:28:59 +0200

    Repo05

--- ex09/src/index5.js
+++
@@ -1,9 +0,0 @@
-function main() {
-    // Only change code below this line
-    var lastName = "Doe";
-    var lastNameLength = "Doe".length;
-    // Only change code above this line
-    return lastNameLength;
-    }
-    console.log(main());
-    module.exports = main;
