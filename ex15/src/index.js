1efcd7b27e7abe42ee07f6dc01fa512542796e65
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-08-09 16:05:22 +0200

    Repo05

---
+++ ex15/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this code
+var myNoun = "dog";
+var myVerb = "ran";
+var myAdjective = "big";
+var myAdverb = "quickly";
+var wordBlanks = "My" + " myNoun" + " was" + " myAdjective" + ", and he " + "myVerb" + " very " + "myAdverb" + ".";
+    // Only change code above this code
+    return wordBlanks;
+    }
+    console.log(main());
+    module.exports = main;
+    
