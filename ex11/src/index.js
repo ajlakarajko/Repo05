450cf427d0d811972c27c5b8284b67242ff1a8f8
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-08-09 15:39:32 +0200

    Repo05

---
+++ ex11/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var myString = "Jeloo World";
+    myString = "Hello World";
+    // Only change code above this line
+    return myString;
+    }
+    console.log(main());
+    module.exports = main;
+    
