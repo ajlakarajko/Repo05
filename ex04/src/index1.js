4a635588ade9e1f7fd163c4d39c30289928f6441
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-08-09 14:55:14 +0200

    Repo05

--- ex04/src/index1.js
+++
@@ -1,7 +0,0 @@
-function main() {
-    // Only change code below this line
-    // Only change code above this line
-    return myString;
-    }
-    console.log(main());
-    module.exports = main;
