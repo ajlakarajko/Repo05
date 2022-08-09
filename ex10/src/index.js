2ba14e3ea7d8381113a11833fa77718915c2459d
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-08-09 15:35:18 +0200

    Repo05

---
+++ ex10/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Donovan";
+    var firstLetterOfLastName = lastName[0]; 
+    // Only change code above this line
+    return firstLetterOfLastName;
+    }
+    console.log(main());
+    module.exports = main;
+    
