c584a3ddb9efdc3b226d86d848f63496511febb7
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-08-09 14:50:37 +0200

    Repo05

---
+++ ex04/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    // Only change code below this line
+    var myString = “FirstLinenewlinetabSecondLinenewlineThirdLine”
+    // Only change code above this line
+    return myString;
+    }
+    console.log(main());
+    module.exports = main;
--- ex04/src/index1.js
+++
@@ -1,8 +0,0 @@
-function main() {
-    // Only change code below this line
-    var myString = “FirstLinenewlinetabSecondLinenewlineThirdLine”
-    // Only change code above this line
-    return myString;
-    }
-    console.log(main());
-    module.exports = main;
